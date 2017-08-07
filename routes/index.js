0c21b901c616e57aa0b9345e6a0c9a1f4b8f1e07
ismael perez <ismaelperezemail@gmail.com>
2017-08-07 08:55:54 -0700

    cleaned up the index.js file. made minor changes to each classroom view

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,19 @@ var fs = require('fs');
 
 var reader = fs.createReadStream('classrooms.json', 'utf8');
 
+/* Read classrooms.json file */
+reader.on('data', function(info) {
+  console.log("new info recieved");
+  mathClassData = JSON.parse(info)[0].students;
+  mathStudentNames = JSON.parse(info)[0].students[0].name;
+  classData = JSON.parse(info);
+  math = classData[0].subject;
+  science = classData[1].subject;
+  music = classData[2].subject;
+  literature = classData[4].subject;
+});
 
-/* GET home page. */
+/* GET home page. */ 
 router.get('/', function(req, res, next) {
   res.render('index', {
     title: 'ACME tech school',
@@ -13,26 +24,70 @@ router.get('/', function(req, res, next) {
   });
 });
 
-
-reader.on('data', function(info) {
-  classData = JSON.parse(info);
-  math = classData[0].subject;
-  science = classData[1].subject;
-  music = classData[2].subject;
-  literature = classData[4].subject;
-
+/* GET classroom page. */
+router.get('/classlist', function(req, res, next) {
+  res.render('classList', {
+    title: 'Class List',
+    classroom: classData,
+    mathClass: math,
+    scienceClass: science,
+    musicClass: music,
+    litClass: literature
+  });
 });
 
+/* GET math page. */
+router.get('/math', function(req, res, next) {
+  res.render('math', {
+    title: 'ACME Math',
+    classroom: mathClassData,
+    students: mathStudentNames,
+    mathClass: math,
+    scienceClass: science,
+    musicClass: music,
+    litClass: literature
+  });
+});
 
-// var subjectGetter = function(){
-//   classData = JSON.parse(info);
-//   var classSub  = "";
-//   for (var i = 0; i < classData.length; i++) {
-//     classSub = classdata[i];
-//   }
-//   return classSub;
-// }
-// subjectGetter();
+/* GET science page. */
+router.get('/science', function(req, res, next) {
+  res.render('science', {
+    title: 'ACME Science',
+    classroom: mathClassData,
+    students: mathStudentNames,
+    mathClass: math,
+    scienceClass: science,
+    musicClass: music,
+    litClass: literature
+  });
+});
+
+/* GET music page. */
+router.get('/music', function(req, res, next) {
+  res.render('music', {
+    title: 'ACME Music',
+    classroom: mathClassData,
+    students: mathStudentNames,
+    mathClass: math,
+    scienceClass: science,
+    musicClass: music,
+    litClass: literature
+  });
+});
+
+/* GET literature page. */
+router.get('/literature', function(req, res, next) {
+  res.render('literature', {
+    title: 'ACME Literature',
+    classroom: mathClassData,
+    students: mathStudentNames,
+    mathClass: math,
+    scienceClass: science,
+    musicClass: music,
+    litClass: literature
+  });
+});
+
 
 
 module.exports = router;
